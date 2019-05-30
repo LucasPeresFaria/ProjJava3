2346d1793dabfa7d84afa1c8f4986e1bbf1c540b
LucasPeresFaria <48368808+LucasPeresFaria@users.noreply.github.com>
2019-05-30 19:56:21 -0300

    Update DaoCliente.java

--- prjPOOMurilo_LucasPeres/src/fatec/poo/control/DaoCliente.java
+++ prjPOOMurilo_LucasPeres/src/fatec/poo/control/DaoCliente.java
@@ -37,6 +37,70 @@ public class DaoCliente {
         }
     }
     
+    public void alterar(Cliente cliente) {
+        PreparedStatement ps = null;
+        try {
+            ps = conn.prepareStatement("UPDATE Cliente set nome = ?,endereco = ?,cidade = ?,uf = ?,cep = ?,ddd = ?,telefone = ?,limiteCred = ?,limiteDisp = ? " +
+                                        "where cpfCliente = ?");
+
+            ps.setString(2,cliente.getNome());
+            ps.setString(3,cliente.getEndereco());
+            ps.setString(4,cliente.getCidade());
+            ps.setString(5,cliente.getUf());
+            ps.setString(6,cliente.getCep());
+            ps.setString(7,cliente.getDdd());
+            ps.setString(8,cliente.getTelefone());
+            ps.setDouble(9,cliente.getLimiteCred());
+            ps.setDouble(10,cliente.getLimiteDisp());
+                      
+            ps.execute();
+        } catch (SQLException ex) {
+             System.out.println(ex.toString());   
+        }
+    }
+    
+     public void excluir(Cliente cliente) {
+        PreparedStatement ps = null;
+        try {
+            ps = conn.prepareStatement("DELETE FROM Cliente where cpfCliente = ?");
+            
+            ps.setString(1, cliente.getCpf());
+                      
+            ps.execute();
+        } catch (SQLException ex) {
+             System.out.println(ex.toString());   
+        }
+    }
+     
+     public  Cliente consultar (String cpf) {
+        Cliente c = null;
+       
+        PreparedStatement ps = null;
+        try {
+            ps = conn.prepareStatement("SELECT * from Cliente where " +
+                                                 "Codigo_Proj = ?");
+            
+            ps.setString(1, cpf);
+            ResultSet rs = ps.executeQuery();
+           
+            if (rs.next()) { //(rs.next() == true) forma redundante de fazer
+                c = new Cliente (cpf, rs.getString("nome"),rs.getDouble("limiteCred"));
+                c.setEndereco(rs.getString("endereco"));
+                c.setCidade(rs.getString("cidade"));
+                c.setUf(rs.getString("uf"));
+                c.setCep(rs.getString("cep"));
+                c.setDdd(rs.getString("ddd"));
+                c.setTelefone(rs.getString("telefone"));
+                c.setLimiteDisp(rs.getDouble("limiteDisp"));
+   
+            }
+        }
+        catch (SQLException ex) { 
+             System.out.println(ex.toString());   
+        }
+        return (c);
+    } 
+    
     
     
 }
